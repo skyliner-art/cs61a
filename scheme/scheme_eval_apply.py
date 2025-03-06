ca50be189777ad16dcbccb870c53e744562a91f5
cskylner <zkylin.cs@gmail.com>
2025-03-07 03:38:19 +0800

    Q3:Refactor scheme_eval_apply.py to improve scheme evaluation

--- scheme/scheme_eval_apply.py
+++ scheme/scheme_eval_apply.py
@@ -34,6 +34,9 @@ def scheme_eval(expr, env, _=None): # Optional third argument is ignored
     else:
         # BEGIN PROBLEM 3
         "*** YOUR CODE HERE ***"
+        operator = scheme_eval(first,env)
+        list_args = rest.map(lambda x:scheme_eval(x,env))
+        return scheme_apply(operator,list_args,env)
         # END PROBLEM 3
 
 def scheme_apply(procedure, args, env):

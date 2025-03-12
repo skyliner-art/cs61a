f8c21a6025b2b6fadd98cab86cb5448a542457cb
cskylner <zkylin.cs@gmail.com>
2025-03-12 15:39:04 +0800

    Q10:Refactor do_define_form to handle defining a named procedure in scheme_forms.py

--- scheme/scheme_forms.py
+++ scheme/scheme_forms.py
@@ -44,6 +44,9 @@ def do_define_form(expressions, env):
         # defining a named procedure e.g. (define (f x y) (+ x y))
         # BEGIN PROBLEM 10
         "*** YOUR CODE HERE ***"
+        validate_formals(signature.rest)
+        env.define(signature.first,LambdaProcedure(signature.rest,expressions.rest,env))
+        return signature.first
         # END PROBLEM 10
     else:
         bad_signature = signature.first if isinstance(signature, Pair) else signature
